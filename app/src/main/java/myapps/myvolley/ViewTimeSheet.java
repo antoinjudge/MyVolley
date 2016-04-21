09b3b6b4887f34fa8af5421609cabee73bbd66aa
antoinjudge <antoin21ok@gmail.com>
2016-04-21 20:23:07 +0100

    //Added functionality to calculate distance using Google Distance Matrix API

---
+++ app/src/main/java/myapps/myvolley/ViewTimeSheet.java
@@ -0,0 +1,141 @@
+package myapps.myvolley;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ViewTimeSheet extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTEmpID;
+    private EditText editTBasic;
+    private EditText editTMeals;
+    private EditText editTMileage;
+    private EditText editTOT;
+    private Button btnPrev;
+    private Button btnNext;
+    private Button btnSave;
+    private Button btnDelete;
+
+    private static final String SELECT_SQL = "SELECT * FROM timesheet";
+    private SQLiteDatabase db;
+
+    private Cursor c;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_time_sheet);
+
+        openDatabase();
+
+        editTEmpID = (EditText) findViewById(R.id.editTextId);
+        editTBasic = (EditText) findViewById(R.id.editTextBasic);
+        editTOT = (EditText) findViewById(R.id.editTextOverTime);
+        editTMeals = (EditText) findViewById(R.id.editTextMeals);
+        editTMileage = (EditText) findViewById(R.id.editTextMileage);
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+
+        btnNext.setOnClickListener(this);
+        btnPrev.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+
+        c = db.rawQuery(SELECT_SQL, null);
+        c.moveToFirst();
+        showRecords();
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+
+    protected void openDatabase() {
+        db = openOrCreateDatabase("TimesheetDB", Context.MODE_PRIVATE, null);
+    }
+
+    protected void showRecords() {
+        String emplid = c.getString(0);
+        String basic = c.getString(1);
+        String overtime = c.getString(2);
+        String meals = c.getString(3);
+        String date = c.getString(4);
+
+        editTEmpID.setText(emplid);
+        editTBasic.setText(basic);
+        editTOT.setText(overtime);
+        editTMeals.setText(meals);
+        editTMileage.setText(date);
+    }
+
+    protected void moveNext() {
+        if (!c.isLast())
+            c.moveToNext();
+
+        showRecords();
+    }
+
+    protected void movePrev() {
+        if (!c.isFirst())
+            c.moveToPrevious();
+
+        showRecords();
+
+    }
+    protected void saveRecord() {
+        String empid = editTEmpID.getText().toString().trim();
+        String basic = editTBasic.getText().toString().trim();
+        String overtime = editTOT.getText().toString().trim();
+
+        String sql = "UPDATE timesheet SET empid='" + empid + "', basic='" + basic + "' WHERE empid=" + empid + ";";
+
+        if (basic.equals("") || overtime.equals("") || empid.equals("")) {
+            Toast.makeText(getApplicationContext(), "You cannot save blank values", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        db.execSQL(sql);
+        Toast.makeText(getApplicationContext(), "Records Saved Successfully", Toast.LENGTH_LONG).show();
+        c = db.rawQuery(SELECT_SQL, null);
+        c.moveToPosition(Integer.parseInt(empid));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnNext) {
+            moveNext();
+        }
+
+        if (v == btnPrev) {
+            movePrev();
+        }
+
+        if (v == btnSave) {
+            saveRecord();
+        }
+
+
+    }
+}
